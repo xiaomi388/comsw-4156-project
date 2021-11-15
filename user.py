a1c08bfc8ec1cd01b26090b41005b9b2f7cf5213
qq544259335 <31943784+qq544259335@users.noreply.github.com>
2021-11-15 09:41:29 -0800

    merge user and userLogin (#18)

--- user.py
+++ user.py
@@ -3,7 +3,11 @@ import json
 import sqlite3
 from wtforms import Form, validators,EmailField, PasswordField, StringField,FormField, IntegerField
 from wtforms.validators import DataRequired,DataRequired, Email,EqualTo,length
-
+import jsonschema
+import json
+import flask
+import sqlite3
+import userDB
 
 '''class USMobileForm(Form):
     area_code = IntegerField('Area Code', validators = [DataRequired(), length(min = 3, max = 3)])
@@ -43,3 +47,30 @@ def register(raw_form):
                 conn.close()
     return json.dumps({"error": form.errors}), 201
     #jsonify(form.errors), 400
+
+def set_user_cookie(email:str, resp):
+    resp.set_cookie('user', email)
+
+def need_login_response():
+    return json.dumps({"error": f"Please login first"}), 400
+
+def user_login(req):
+    # integrity check
+    print(req.endpoint)
+    try:
+        email = req.args.get("email")
+        password = req.args.get("password")
+        print(email, password)
+        saved_user = userDB.select_user_by_email(email)
+        print(saved_user)
+        if not saved_user:
+            return json.dumps({"error": f"No such email {str(email)}"}), 400
+        # To Do: md5
+        if not password == saved_user.get_password():
+            return json.dumps({"error": f"wrong password {str(email)}"}), 400
+        resp = flask.make_response(json.dumps({"error": ""}))
+        set_user_cookie(email, resp)
+        return resp, 200
+    except :
+        return json.dumps({"error": f"Internal error"}), 500
+
